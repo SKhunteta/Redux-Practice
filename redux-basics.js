8df8a737fdb3ffe7a170911b5d2efc5396d5d5f5
SKhunteta <shreyans.khunteta@gmail.com>
2019-09-24 13:39:42 -0700

    dispatching actions

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,20 @@ const initialState = {
 
 //Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        state.counter++;
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'ADD_COUNTER') {
+        state.counter++;
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
     return state;
 };
 
@@ -16,5 +30,9 @@ console.log(store.getState());
 
 
 //Dispatching action
+
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
 //Subscription
