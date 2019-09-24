a47fadc5dce9326af8630f4d2159783f1a27e118
SKhunteta <shreyans.khunteta@gmail.com>
2019-09-24 13:45:15 -0700

    Added subscriptions

--- redux-basics.js
+++ redux-basics.js
@@ -28,6 +28,11 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+//Subscription
+
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+});
 
 //Dispatching action
 
@@ -35,4 +40,3 @@ store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
 console.log(store.getState());
 
-//Subscription
